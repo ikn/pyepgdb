91134c888fd4a0c2b979771d058673be31d9cec6
Joseph Lansdowne <j49137@gmail.com>
2019-08-03 19:49:29 +0100

    documentation: warn bad links, link to Python docs

--- doc/conf.py
+++ doc/conf.py
@@ -7,13 +7,23 @@ project = 'pyepgdb'
 copyright = '2019, Joseph Lansdowne'
 author = 'Joseph Lansdowne'
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'haiku'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx'
+]
+nitpicky = True
 
 autodoc_default_options = {
     'member-order': 'bysource',
     'members': True,
     'show-inheritance': True,
     'undoc-members': True,
+}
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
 }
